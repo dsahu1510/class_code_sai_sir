e8587cc86a09f592e07eedcba9decfe1902f3f0f
Devyani <devyanisahu1@gmail.com>
2022-01-07 21:02:58 +0530

    added

---
+++ 6Jan2022/index.js
@@ -0,0 +1,12 @@
+
+var a=10,b=20,c=40;
+
+console.log(a < b && a < c);
+
+console.log(a < b && a > c);
+
+console.log(a > b || a > c);
+
+console.log(a > b || a < c);
+
+
---
+++ 7Jan2022/index.js
@@ -0,0 +1,10 @@
+var course="mean";
+
+switch(course){
+    case "java": console.log("java course"); break;
+    case "dotnet": console.log("dotnet course"); break;
+    case "mean": console.log("mean course"); break;
+    case "mern": console.log("mern course"); break;
+    default : console.log("no course avaible");
+
+}
